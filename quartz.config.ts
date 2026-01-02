f228d9cea5e2dfc1e3c70ccc86c17beab2424ba8
teeeswift <chung.taylor1@gmail.com>
2026-01-02 22:20:39 +0900

    Quartz sync: Jan 2, 2026, 10:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "teeeswift.github.io/hayama/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
